8630fe3a2ebdc5f6186f4bb780bf699f0c46e392
Pranjali Hajare <pranjalihajare2002@gmail.com>
2024-05-15 17:53:59 +0530

    first commit

---
+++ src/constants/constant.js
@@ -0,0 +1,7 @@
+
+export const logo = "https://dcassetcdn.com/design_img/10150/16906/16906_294312_10150_image.jpg"
+
+
+export const API_URL = "https://forkify-api.herokuapp.com/api";
+
+// https://ourrecipes.in/wp-content/uploads/2021/09/our-recipes-logo-retina.png
